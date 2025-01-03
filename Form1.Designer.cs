116d2f77f94a980cbb5848249d2e773f16c1e5ba
anhquang03 <2billcipher@gmail.com>
2025-01-03 16:45:05 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            cbotinhtrang = new ComboBox();
+            label2 = new Label();
+            label5 = new Label();
+            txtsoluong = new TextBox();
+            label4 = new Label();
+            txtgiatien = new TextBox();
+            label3 = new Label();
+            txttensanpham = new TextBox();
+            txtmasanpham = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox2 = new GroupBox();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(cbotinhtrang);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(txtsoluong);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(txtgiatien);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(txttensanpham);
+            groupBox1.Controls.Add(txtmasanpham);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(41, 29);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(780, 229);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Nhập thông tin sản phẩm";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // cbotinhtrang
+            // 
+            cbotinhtrang.FormattingEnabled = true;
+            cbotinhtrang.Items.AddRange(new object[] { "Còn hàng", "Hết hàng" });
+            cbotinhtrang.Location = new Point(629, 63);
+            cbotinhtrang.Name = "cbotinhtrang";
+            cbotinhtrang.Size = new Size(133, 31);
+            cbotinhtrang.TabIndex = 10;
+            cbotinhtrang.SelectedIndexChanged += cbotinhtrang_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F);
+            label2.Location = new Point(25, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Tên Sản phẩm";
+            label2.Click += label2_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F);
+            label5.Location = new Point(534, 66);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 23);
+            label5.TabIndex = 9;
+            label5.Text = "Trạng thái";
+            label5.Click += label5_Click;
+            // 
+            // txtsoluong
+            // 
+            txtsoluong.Location = new Point(389, 157);
+            txtsoluong.Name = "txtsoluong";
+            txtsoluong.Size = new Size(125, 30);
+            txtsoluong.TabIndex = 8;
+            txtsoluong.TextChanged += txtsoluong_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F);
+            label4.Location = new Point(303, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 23);
+            label4.TabIndex = 7;
+            label4.Text = "Số lượng";
+            label4.Click += label4_Click;
+            // 
+            // txtgiatien
+            // 
+            txtgiatien.Location = new Point(389, 60);
+            txtgiatien.Name = "txtgiatien";
+            txtgiatien.Size = new Size(125, 30);
+            txtgiatien.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F);
+            label3.Location = new Point(303, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 23);
+            label3.TabIndex = 5;
+            label3.Text = "Giá tiền";
+            label3.Click += label3_Click;
+            // 
+            // txttensanpham
+            // 
+            txttensanpham.Location = new Point(151, 157);
+            txttensanpham.Name = "txttensanpham";
+            txttensanpham.Size = new Size(125, 30);
+            txttensanpham.TabIndex = 4;
+            // 
+            // txtmasanpham
+            // 
+            txtmasanpham.Location = new Point(151, 60);
+            txtmasanpham.Name = "txtmasanpham";
+            txtmasanpham.Size = new Size(125, 30);
+            txtmasanpham.TabIndex = 2;
+            txtmasanpham.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(25, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 23);
+            label1.TabIndex = 1;
+            label1.Text = "Mã SP";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            button1.Location = new Point(41, 280);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 64);
+            button1.TabIndex = 3;
+            button1.Text = "Thêm sản phẩm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            button2.Location = new Point(242, 280);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 64);
+            button2.TabIndex = 5;
+            button2.Text = "Sửa sản phẩm";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            button3.Location = new Point(447, 280);
+            button3.Name = "button3";
+            button3.Size = new Size(166, 64);
+            button3.TabIndex = 6;
+            button3.Text = "Xóa sản phẩm";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            button4.Location = new Point(648, 280);
+            button4.Name = "button4";
+            button4.Size = new Size(173, 64);
+            button4.TabIndex = 7;
+            button4.Text = "Thoát chương trình";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(listView1);
+            groupBox2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(41, 365);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(780, 340);
+            groupBox2.TabIndex = 8;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin chi tiết sản phẩm";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(11, 50);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(751, 275);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged_1;
+            listView1.Click += listView1_Click;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Mã SP";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Tên Sản Phẩm";
+            columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Giá tiền";
+            columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Số lượng";
+            columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Trạng Thái";
+            columnHeader5.Width = 120;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(873, 731);
+            Controls.Add(groupBox2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Phần mềm quản lý đồng hồ";
+            Load += Form1_Load;
+            Click += Form1_Click;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox txtmasanpham;
+        private Button button1;
+        private Label label5;
+        private TextBox txtsoluong;
+        private Label label4;
+        private TextBox txtgiatien;
+        private Label label3;
+        private TextBox txttensanpham;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private GroupBox groupBox2;
+        private ComboBox cbotinhtrang;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,259 @@
+﻿using System.Data.SqlClient;
+namespace WinFormsApp1
+    
+{
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String chuoiketnoi = @"Data Source=WIN-ALRJI68AQDD\ANHQUANG03;Initial Catalog=quanlydongho;Integrated Security=True;Encrypt=False";
+        String sql;
+        System.Data.SqlClient.SqlConnection ketnoi;
+        System.Data.SqlClient.SqlCommand thuchien;
+        System.Data.SqlClient.SqlDataReader docdulieu;
+        int i = 0;
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult f = MessageBox.Show("Bạn có muốn thoát chương trình không ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (f == DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ketnoi = new System.Data.SqlClient.SqlConnection(chuoiketnoi);
+            hienthi();
+
+        }
+        public void hienthi()
+        {
+            listView1.Items.Clear();
+            ketnoi.Open();
+            sql = @"Select Mã_SP, Tên_SP, Giá_Tiền, Số_Lượng, Trạng_thái FROM sanpham";
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            docdulieu = thuchien.ExecuteReader();
+            i = 0;
+            while (docdulieu.Read())
+            {
+                listView1.Items.Add(docdulieu[0].ToString());
+                listView1.Items[i].SubItems.Add(docdulieu[1].ToString());
+                listView1.Items[i].SubItems.Add(docdulieu[2].ToString());
+                listView1.Items[i].SubItems.Add(docdulieu[3].ToString());
+                listView1.Items[i].SubItems.Add(docdulieu[4].ToString());
+                i++;
+            }
+            ketnoi.Close();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            txtmasanpham.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            txttensanpham.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtgiatien.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            txtsoluong.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            cbotinhtrang.Text = listView1.SelectedItems[0].SubItems[4].Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Danh sách các ô bị bỏ trống
+            List<string> missingFields = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(txtmasanpham.Text))
+                missingFields.Add("Mã sản phẩm");
+            if (string.IsNullOrWhiteSpace(txttensanpham.Text))
+                missingFields.Add("Tên sản phẩm");
+            if (string.IsNullOrWhiteSpace(txtgiatien.Text))
+                missingFields.Add("Giá tiền");
+            if (string.IsNullOrWhiteSpace(txtsoluong.Text))
+                missingFields.Add("Số lượng");
+            if (string.IsNullOrWhiteSpace(cbotinhtrang.Text))
+                missingFields.Add("Trạng thái");
+
+
+            // Nếu có trường bị bỏ trống
+            if (missingFields.Count > 0)
+            {
+                string errorMessage = "Các ô sau không được bỏ trống:\n- " + string.Join("\n- ", missingFields);
+                MessageBox.Show(errorMessage, "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+
+            listView1.Items.Clear();
+            ketnoi.Open();
+
+            // Kiểm tra trùng lặp Mã sản phẩm
+            sql = @"SELECT COUNT(*) FROM sanpham WHERE Mã_SP = N'" + txtmasanpham.Text + @"'";
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            int countMaSP = (int)thuchien.ExecuteScalar();
+
+            // Kiểm tra trùng lặp Tên sản phẩm
+            sql = @"SELECT COUNT(*) FROM sanpham WHERE Tên_SP = N'" + txttensanpham.Text + @"'";
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            int countTenSP = (int)thuchien.ExecuteScalar();
+
+            // Nếu trùng Mã sản phẩm hoặc Tên sản phẩm
+            if (countMaSP > 0 || countTenSP > 0)
+            {
+                string errorMessage = "Dữ liệu bị lặp:\n";
+                if (countMaSP > 0)
+                    errorMessage += "- Mã sản phẩm đã tồn tại.\n";
+                if (countTenSP > 0)
+                    errorMessage += "- Tên sản phẩm đã tồn tại.";
+                MessageBox.Show(errorMessage, "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                ketnoi.Close();
+                hienthi();
+                return; // Thoát khỏi hàm nếu có trùng lặp
+                
+            }
+
+
+            sql = @"Insert into sanpham(Mã_SP, Tên_SP, Giá_Tiền, Số_Lượng, Trạng_thái)
+Values      ( N'" + txtmasanpham.Text + @"',N'" + txttensanpham.Text + @"',N'" + txtgiatien.Text + @"',N'" + txtsoluong.Text + @"',N'" + cbotinhtrang.Text + @"')";
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            thuchien.ExecuteNonQuery();
+            ketnoi.Close();
+            hienthi();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra nếu trạng thái là "Còn hàng" nhưng ô số lượng bị trống
+            if (cbotinhtrang.SelectedItem != null && cbotinhtrang.SelectedItem.ToString() == "Còn hàng" && string.IsNullOrWhiteSpace(txtsoluong.Text))
+            {
+                MessageBox.Show("Ô Số lượng không được để trống khi trạng thái là 'Còn hàng'.", "Lỗi dữ liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Dừng chương trình để người dùng sửa dữ liệu
+            }
+
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm để sửa.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Thoát khỏi hàm nếu không có mục nào được chọn
+            }
+
+
+
+
+            ketnoi.Open();
+            sql = @"Update sanpham SET
+                    Mã_SP = N'" + txtmasanpham.Text + @"', Tên_SP = N'" + txttensanpham.Text + @"',
+                    Giá_Tiền = N'" + txtgiatien.Text + @"' , Số_Lượng = N'" + txtsoluong.Text + @"',
+                    Trạng_thái = N'" + cbotinhtrang.Text + @"'
+Where       (Mã_SP = N'" + txtmasanpham.Text + @"')";
+
+
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            thuchien.ExecuteNonQuery();
+            ketnoi.Close();
+            hienthi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            ketnoi.Open();
+            sql = @"Delete from sanpham Where (Mã_SP = N'" + txtmasanpham.Text + @"')";
+            thuchien = new System.Data.SqlClient.SqlCommand(sql, ketnoi);
+            thuchien.ExecuteNonQuery();
+            ketnoi.Close();
+            hienthi();
+        }
+
+        private void cbotinhtrang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra giá trị được chọn
+            if (cbotinhtrang.SelectedItem != null && cbotinhtrang.SelectedItem.ToString() == "Hết hàng")
+            {
+                txtsoluong.Text = "0"; // Đặt số lượng về 0
+                txtsoluong.Enabled = false; // Vô hiệu hóa ô nhập số lượng
+            }
+            else
+            {
+                txtsoluong.Enabled = true; // Kích hoạt lại ô nhập số lượng
+                txtsoluong.Text = ""; // Xóa nội dung ô nhập
+            }
+        }
+
+        private void txtsoluong_TextChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra xem giá trị của ô số lượng là 0
+            if (txtsoluong.Text == "0")
+            {
+                cbotinhtrang.SelectedItem = "Hết hàng"; // Tự động chọn trạng thái là "Hết hàng"
+                txtsoluong.Enabled = false; // Vô hiệu hóa ô số lượng
+            }
+            else
+            {
+                if (cbotinhtrang.SelectedItem != null && cbotinhtrang.SelectedItem.ToString() == "Hết hàng")
+                {
+                    cbotinhtrang.SelectedIndex = -1; // Bỏ chọn trạng thái "Hết hàng"
+                }
+                txtsoluong.Enabled = true; // Kích hoạt lại ô số lượng
+            }
+        }
+    }
+}
